9c6e53ab134fd48cca8e361d407d9f1d7acb519a
PlinyTheYounger0 <PlinyTheYounger0@proton.me>
2026-05-04 11:03:14 -0700

    Process videos for Fast Start

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -20,9 +20,11 @@ import (
 )
 
 func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	//Maximum video upload size is 1GB
 	maxFileSize := int64(1 << 30)
 	r.Body = http.MaxBytesReader(w, r.Body, maxFileSize)
 
+	//Parse video and authenticate user/user authoization
 	videoIDString := r.PathValue("videoID")
 	videoID, err := uuid.Parse(videoIDString)
 	if err != nil {
@@ -41,6 +43,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusUnauthorized, "Failed to Authenticate User to Upload Video.", err)
 		return
 	}
+	log.Print("User Authenticated")
 
 	dbVideo, err := cfg.db.GetVideo(videoID)
 	if err != nil {
@@ -52,6 +55,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusUnauthorized, "Cannot Edit Video You Don't Own.", err)
 		return
 	}
+	log.Print("User Authroized to Edit Video")
 
 	file, header, err := r.FormFile("video")
 	if err != nil {
@@ -59,12 +63,14 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 	defer file.Close()
+	log.Print("Request Parsed")
 
 	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
 	if mediaType != "video/mp4" || err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Error Parsing Content-Type to Upload Video.", err)
 		return
 	} 
+	log.Print("Media Type Determined")
 
 	tempFile, err := os.CreateTemp("", "tubely-upload.mp4")
 	if err != nil {
@@ -73,19 +79,23 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	}
 	defer os.Remove(tempFile.Name())
 	defer tempFile.Close()
+	log.Print("tempFile created")
 
 	_, err = io.Copy(tempFile, file)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Failed to Copy Video Upload to Temp File.", err)
 		return
 	}
+	log.Print("File Copied Successfully")
 
-	_, err = tempFile.Seek(0, io.SeekStart)
+	//Generate AWS prefix for video based on Aspect Ratio
+	prefix, err := getVideoAspectRatio(tempFile.Name())
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Failed to Return to Start of Video Upload Temp File.", err)
+		respondWithError(w, http.StatusInternalServerError, "Failed to Get Video Prefix.", err)
 		return
 	}
 
+	//Make a random 32 byte base64 filename for the key
 	bytes := make([]byte, 32)
 	_, err = rand.Read(bytes)
 	if err != nil {
@@ -93,21 +103,35 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	prefix, err := getVideoAspectRatio(tempFile.Name())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Failed to Get Video Prefix.", err)
-		return
-	}
 	magicFileName := base64.RawURLEncoding.EncodeToString(bytes)
 	key := fmt.Sprintf("%s/%s.mp4",prefix, magicFileName)
+	log.Printf("%s Set as Key", key)
 
+	//Process Video for Fast Start
+	processedTempFilePath, err := processVideoForFastStart(tempFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to Process Video for Fast Start.", err)
+		return
+	}
+
+	processedTempFile, err := os.Open(processedTempFilePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to Open Processed Video.", err)
+	}
+	defer os.Remove(processedTempFilePath)
+	defer processedTempFile.Close()
+
+	//Insert the processed video into s3
+	log.Print("Video Starting the be uploaded to s3")
 	_, err = cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
 		Bucket: &cfg.s3Bucket,
 		Key: &key,
-		Body: tempFile,
+		Body: processedTempFile,
 		ContentType: &mediaType,
 	})
+	log.Print("Video Successfully uploaded to s3")
 
+	//Update the video url in the database 
 	videoURL :=  fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, key)
 	log.Printf("Video Uploaded to URL: %s\n", videoURL)
 	dbVideo.VideoURL = &videoURL
@@ -154,22 +178,34 @@ func getVideoAspectRatio(filepath string) (string, error) {
 	
 	width := result.Streams[0].Width
 	height := result.Streams[0].Height
-	fmt.Printf("Width: %d, Height: %d\n", width, height)  
+	fmt.Printf("Video Width: %d, Video Height: %d\n", width, height)  
 
 	aspectRatio := float64(width) / float64(height)
 	switch {
 	case math.Abs(aspectRatio - 1.777) < 0.1:
 		videoAspectRatio = "landscape"
-		fmt.Printf("Assigned Aspect Ratio: %s\n", videoAspectRatio)
+		log.Printf("Assigned Aspect Ratio: %s\n", videoAspectRatio)
 	case math.Abs(aspectRatio - 0.5625) < 0.1:
 		videoAspectRatio = "portrait"
-		fmt.Printf("Assigned Aspect Ratio: %s\n", videoAspectRatio)
+		log.Printf("Assigned Aspect Ratio: %s\n", videoAspectRatio)
 	default:
 		videoAspectRatio = "other"
-		fmt.Printf("Assigned Aspect Ratio: %s\n", videoAspectRatio)
+		log.Printf("Assigned Aspect Ratio: %s\n", videoAspectRatio)
 	}
 
 	return videoAspectRatio, nil
 }
 
-func processVideoForFastStart(filePath string) (string, error) 
+func processVideoForFastStart(filePath string) (string, error) {
+	outputFilePath := filePath + ".processing"
+
+	fastStartEncoder := exec.Command("ffmpeg", "-i", filePath, "-c", "copy", "-movflags", "faststart", "-f", "mp4", outputFilePath)
+	err := fastStartEncoder.Run()
+	if err != nil {
+		return "", err
+	}
+	log.Printf("%s Processed for Fast Start.\n", outputFilePath)
+
+	return outputFilePath, nil
+
+}
